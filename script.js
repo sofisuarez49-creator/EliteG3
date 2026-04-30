7cce096433bcd8a1d2dd5078ab5407ac9b2c78f5
sofisuarez49-creator <sofisuarez49@gmail.com>
2026-04-30 16:04:35 -0300

    Agregar fallback de inicialización Firebase en script principal

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-        // Configuración de tu proyecto de Firebase
-        const firebaseConfig = {
-            apiKey: "AIzaSyAcW679YdIT03pyq8IgKKnR8oyO4_fvpFs",
-            authDomain: "g2elite-a3b85.firebaseapp.com",
-            databaseURL: "https://g2elite-a3b85-default-rtdb.firebaseio.com/",
-            projectId: "g2elite-a3b85",
-            storageBucket: "g2elite-a3b85.firebasestorage.app",
-            messagingSenderId: "951346482286",
-            appId: "1:951346482286:web:6001135252de2a4d982a7f",
+(async () => {
+        const getFirebaseDb = async () => {
+            const existingDb = await (window.firebaseDbReady || Promise.resolve(window.firebaseDb));
+            if (existingDb) return existingDb;
+
+            const [{ initializeApp, getApps, getApp }, { getDatabase, ref, onValue, off, get, set, update, remove, push }] = await Promise.all([
+                import('https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js'),
+                import('https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js')
+            ]);
+
+            const firebaseConfig = {
+                apiKey: "AIzaSyAcW679YdIT03pyq8IgKKnR8oyO4_fvpFs",
+                authDomain: "g2elite-a3b85.firebaseapp.com",
+                databaseURL: "https://g2elite-a3b85-default-rtdb.firebaseio.com/",
+                projectId: "g2elite-a3b85",
+                storageBucket: "g2elite-a3b85.firebasestorage.app",
+                messagingSenderId: "951346482286",
+                appId: "1:951346482286:web:6001135252de2a4d982a7f",
+            };
+
+            const createSnapshot = (data) => ({ val: () => data });
+            const createRefCompat = (nativeDb, path) => {
+                const queryRef = ref(nativeDb, path);
+                const valueCallbacks = new Map();
+                return {
+                    once: (eventName) => {
+                        if (eventName !== 'value') throw new Error(`Evento no soportado: ${eventName}`);
+                        return get(queryRef).then((snap) => createSnapshot(snap.val()));
+                    },
+                    on: (eventName, callback) => {
+                        if (eventName !== 'value') throw new Error(`Evento no soportado: ${eventName}`);
+                        const unsubscribe = onValue(queryRef, (snap) => callback(createSnapshot(snap.val())));
+                        valueCallbacks.set(callback, unsubscribe);
+                        return callback;
+                    },
+                    off: (eventName, callback) => {
+                        if (eventName && eventName !== 'value') return;
+                        if (callback && valueCallbacks.has(callback)) {
+                            valueCallbacks.get(callback)();
+                            valueCallbacks.delete(callback);
+                            return;
+                        }
+                        valueCallbacks.forEach((unsubscribe) => unsubscribe());
+                        valueCallbacks.clear();
+                        off(queryRef);
+                    },
+                    set: (value) => set(queryRef, value),
+                    update: (value) => update(queryRef, value),
+                    remove: () => remove(queryRef),
+                    push: (value) => {
+                        const newRef = push(queryRef);
+                        return value === undefined ? Promise.resolve(newRef) : set(newRef, value).then(() => newRef);
+                    }
+                };
+            };
+
+            const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+            const nativeDb = getDatabase(app);
+            const dbCompat = { ref: (path) => createRefCompat(nativeDb, path) };
+            window.firebaseDb = dbCompat;
+            window.firebase = { database: () => dbCompat };
+            return dbCompat;
         };
 
-        // Inicializar Firebase
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-        }
-        const db = firebase.database();
+        const db = await getFirebaseDb();
         const { useState, useEffect, useMemo, useRef } = React;
 
         const GALLERY_LABELS = ['C', 'P', 'B', 'N', 'S', 'E', 'X'];
@@ -6811,3 +6860,5 @@ const saveProfile = (e) => {
 
         const root = ReactDOM.createRoot(document.getElementById('root'));
         root.render(<App />);
+
+})();
